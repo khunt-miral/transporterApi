9ef6a1da50b29bceecdb2cea0d0ea77771816852
Khunt Miral <20ce051.miral.khunt@vvpedulink.ac.in>
2023-02-22 22:50:11 +0530

    firebase verification

--- TransporterAPI/src/main/java/com/springboot/TransporterApiApplication.java
+++ TransporterAPI/src/main/java/com/springboot/TransporterApiApplication.java
@@ -3,11 +3,12 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.io.IOException;
 
+@SpringBootApplication
 public class TransporterApiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(TransporterApiApplication.class, args);
 	}
 }
