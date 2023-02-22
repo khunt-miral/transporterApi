dff22f0f6ed66a596ed9b628bee6797aee55e423
Khunt Miral <20ce051.miral.khunt@vvpedulink.ac.in>
2023-02-22 22:48:16 +0530

    firebase verification

---
+++ TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/Entity.java
@@ -0,0 +1,11 @@
+package com.springboot.FirebaseUserApi;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Entity {
+    private String uid;
+    private String phoneNumber;
+}
---
+++ TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/FirebaseController.java
@@ -0,0 +1,22 @@
+package com.springboot.FirebaseUserApi;
+
+import com.google.firebase.auth.FirebaseAuthException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class FirebaseController {
+
+    @Autowired
+    public Service service;
+
+    @GetMapping("/phoneNo/{number}")
+    public String getUserByPhoneNO(@PathVariable String number) throws InterruptedException, ExecutionException, FirebaseAuthException {
+        return service.getCrud(number);
+    }
+
+}
