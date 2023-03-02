5ef1adb9936d9c6649e851135e4f52598556e539
Khunt Miral <20ce051.miral.khunt@vvpedulink.ac.in>
2023-03-02 21:21:38 +0530

    Update FirebaseConfig.java

--- TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/FirebaseConfig.java
+++ TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/FirebaseConfig.java
@@ -14,14 +14,20 @@ import java.net.URL;
 @Configuration
 public class FirebaseConfig {
     public FirebaseConfig(@Value("${FIREBASE_URL}") String firebaseUrl) throws IOException {
+        
+        //getting url of json file
         URL url = new URL(firebaseUrl);
+        
+        //getting json file as a stream 
         InputStream refreshToken = url.openStream();
 
+        //authentication with firebase
         FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(refreshToken))
                     .setDatabaseUrl("https://shipperwebapp-default-rtdb.asia-southeast1.firebasedatabase.app")
                     .build();
-
+    
+        //initializing the app
         FirebaseApp.initializeApp(options);
     }
 }
