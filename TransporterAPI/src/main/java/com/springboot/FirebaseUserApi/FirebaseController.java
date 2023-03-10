cb58b4ee2d3a34cedc28c88cdc7a5a3910fbc7b7
Restyled.io <commits@restyled.io>
2023-03-10 16:10:07 +0000

    Restyled by clang-format

--- TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/FirebaseController.java
+++ TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/FirebaseController.java
@@ -1,23 +1,21 @@
 package com.springboot.FirebaseUserApi;
 
 import com.google.firebase.auth.FirebaseAuthException;
+import java.util.concurrent.ExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ExecutionException;
-
 @RestController
 public class FirebaseController {
 
-    @Autowired
-    public Service service;
+  @Autowired public Service service;
 
-    @GetMapping("/phoneNo/{number}")
-    public String getUserByEmail(@PathVariable String number) throws InterruptedException, ExecutionException, FirebaseAuthException {
-       //returning uid as a response       
-       return service.getByEmail(number);
-    }
-
+  @GetMapping("/phoneNo/{number}")
+  public String getUserByEmail(@PathVariable String number)
+      throws InterruptedException, ExecutionException, FirebaseAuthException {
+    // returning uid as a response
+    return service.getByEmail(number);
+  }
 }
