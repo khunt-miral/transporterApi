f932d45849fb3c285641bf38ad927642bd762ba2
Restyled.io <commits@restyled.io>
2023-03-10 16:13:37 +0000

    Restyled by clang-format

--- TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/Service.java
+++ TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/Service.java
@@ -4,14 +4,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
 
-
-
 @org.springframework.stereotype.Service
 public class Service {
 
-    //getting uid by phone number
-    public String getByEmail(String email) throws  FirebaseAuthException {
-        UserRecord userRecord = FirebaseAuth.getInstance().getUserByEmail(email);
-           return userRecord.getUid();
-    }
+  // getting uid by phone number
+  public String getByEmail(String email) throws FirebaseAuthException {
+    UserRecord userRecord = FirebaseAuth.getInstance().getUserByEmail(email);
+    return userRecord.getUid();
+  }
 }
