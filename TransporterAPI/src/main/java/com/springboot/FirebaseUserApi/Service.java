140ca6f5f1c717e148a7e09778717e1385af6c6c
Khunt Miral <20ce051.miral.khunt@vvpedulink.ac.in>
2023-03-11 11:59:29 +0530

    Update Service.java

--- TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/Service.java
+++ TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/Service.java
@@ -9,9 +9,15 @@ import com.google.firebase.auth.UserRecord;
 @org.springframework.stereotype.Service
 public class Service {
 
-    //getting uid by phone number
+    //getting uid by email
     public String getByEmail(String email) throws  FirebaseAuthException {
         UserRecord userRecord = FirebaseAuth.getInstance().getUserByEmail(email);
+           return userRecord.getUid();
+    }
+    
+    //getting uid by phone number
+    public String getByPhoneno(String phoneno) throws  FirebaseAuthException {
+        UserRecord userRecord = FirebaseAuth.getInstance().getUserByPhoneNumber(phoneno);
            return userRecord.getUid();
     }
 }
