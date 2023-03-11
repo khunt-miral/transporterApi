4ccc9e063f5b9a80020460ecccd428a8ef0cea7b
Restyled.io <commits@restyled.io>
2023-03-11 06:27:53 +0000

    Restyled by clang-format

--- TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/FirebaseController.java
+++ TransporterAPI/src/main/java/com/springboot/FirebaseUserApi/FirebaseController.java
@@ -1,31 +1,30 @@
 package com.springboot.FirebaseUserApi;
 
 import com.google.firebase.auth.FirebaseAuthException;
+import java.util.concurrent.ExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ExecutionException;
-
 @RestController
 public class FirebaseController {
 
-    @Autowired
-    public Service service;
-    
-    //get uid from email
-    @GetMapping("/email/{email}")
-    public String getUserByEmail(@PathVariable String email) throws InterruptedException, ExecutionException, FirebaseAuthException {
-       //returning uid as a response       
-       return service.getByEmail(email);
-    }
-    
-    //get uid from phoneno
-    @GetMapping("/phoneNo/{phoneno}")
-    public String getUserByPhoneno(@PathVariable String phoneno) throws InterruptedException, ExecutionException, FirebaseAuthException {
-       //returning uid as a response       
-       return service.getByPhoneno(phoneno);
-    }
+  @Autowired public Service service;
 
+  // get uid from email
+  @GetMapping("/email/{email}")
+  public String getUserByEmail(@PathVariable String email)
+      throws InterruptedException, ExecutionException, FirebaseAuthException {
+    // returning uid as a response
+    return service.getByEmail(email);
+  }
+
+  // get uid from phoneno
+  @GetMapping("/phoneNo/{phoneno}")
+  public String getUserByPhoneno(@PathVariable String phoneno)
+      throws InterruptedException, ExecutionException, FirebaseAuthException {
+    // returning uid as a response
+    return service.getByPhoneno(phoneno);
+  }
 }
